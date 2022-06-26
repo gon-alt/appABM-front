13165223f653338f3e48a4e19b339a7b97c16559
gon-alt <82797848+gon-alt@users.noreply.github.com>
2022-06-26 20:40:02 -0300

    primer commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hola mundo!</h1>
+      <Link href='/products/create'>Crear producto</Link>
+    </div>
+  )
+}
