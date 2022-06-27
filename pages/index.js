4a562ef9b21853402bf896415747c5e1f2e8f665
gon-alt <82797848+gon-alt@users.noreply.github.com>
2022-06-26 21:26:03 -0300

    nada!

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <h1>app-productos</h1>
       <Link href='/products/create'>Crear producto</Link>
     </div>
   )
