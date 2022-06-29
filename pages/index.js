cc7853c40ebdcb6ba75bd8d65848c962d3142e5d
gon-alt <82797848+gon-alt@users.noreply.github.com>
2022-06-29 01:01:49 -0300

    agregando vista de base de datos

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,34 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <div>
+
       <h1>app-productos</h1>
-      <Link href='/products/create'>Crear producto</Link>
-      <Link href='/products/create'>Ver base de datos</Link>
-    </div>
+        <button><h2><Link href='/products/create'>Crear producto</Link></h2></button>
+        <button><h2><Link href="https://appabm.azurewebsites.net/api/v1/products">Ver base de datos</Link></h2></button> 
+
+      </div>
+      <style jsx>
+        {`
+          button {
+             text-align: center;
+            display: flex;
+            flex-direction: column;
+            width: 20rem;
+            margin: 0 auto;
+           
+          }
+
+          input {
+            margin-bottom: 0.5rem;
+          }
+
+          h1 {
+            text-align: center;
+          }       
+        `}
+      </style>
+    </>
   )
 }
