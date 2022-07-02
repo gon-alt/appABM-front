ea91f758652ccc967dae53f0b38f7ebd03cf1577
gon-alt <82797848+gon-alt@users.noreply.github.com>
2022-07-01 23:21:29 -0300

    url de getDB

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 
       <h1>app-productos</h1>
         <button><h2><Link href='/products/create'>Crear producto</Link></h2></button>
-        <button><h2><Link href="https://appabm.azurewebsites.net/api/v1/products">Ver base de datos</Link></h2></button> 
+        <button><h2><Link href="/products/getDB">Ver base de datos</Link></h2></button> 
 
       </div>
       <style jsx>
